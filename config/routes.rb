d89e75d3e9042eace14dc615aa0ce6c5b4808b2b
IAmTechieChic <ericab@iamtechiechic.com>
2014-05-03 15:27:59 -0400

    added Reservation model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'reservation/add'
+
+  get 'reservation/update'
+
+  get 'reservation/cancel'
+
+  get 'reservation/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
